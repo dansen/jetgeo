08511fcbdb207d7fb9f84b8e812f366fab0e3691
dansen <370283593@qq.com>
2025-09-26 17:52:26 +0800

    11

--- jetgeo-core/src/test/java/com/ling5821/jetgeo/JetGeoExample.java
+++ jetgeo-core/src/test/java/com/ling5821/jetgeo/JetGeoExample.java
@@ -14,12 +14,18 @@ public class JetGeoExample {
 
     static {
         JetGeoProperties properties = new JetGeoProperties();
-        properties.setGeoDataParentPath(
-            "D:\\Projects\\idea\\jetgeo\\jetgeo-core\\src\\main\\resources");
-        properties.setLevel(LevelEnum.province);
-        properties.setLevel(LevelEnum.city);
+        // Resolve geo data path: prefer environment variable GEO_DATA_PATH, else fallback to ./data/geodata
+        String envPath = System.getenv("GEO_DATA_PATH");
+        if (envPath == null || envPath.trim().isEmpty()) {
+            // relative to project root when running via maven (user.dir)
+            String userDir = System.getProperty("user.dir");
+            envPath = userDir + java.io.File.separator + "data" + java.io.File.separator + "geodata";
+        }
+        properties.setGeoDataParentPath(envPath);
+        // Set finest level you want to load (district loads all)
         properties.setLevel(LevelEnum.district);
         jetGeo = new JetGeo(properties);
+        System.out.println("[JetGeoExample] Using GEO_DATA_PATH=" + envPath);
     }
 
     public static void main(String[] args) {
